2b87e064bc5af6ec1613726fcca7cf323ff09eed
yulyarudenko88 <111283521+yulyarudenko88@users.noreply.github.com>
2023-05-29 23:32:40 +0300

    ADD: function updateContact

--- models/contacts.js
+++ models/contacts.js
@@ -64,7 +64,23 @@ async function addContact(name, email, phone) {
 }
 
 
-const updateContact = async (contactId, body) => {}
+const updateContact = async (contactId, name, email, phone) => {
+  try {
+    const contacts = await listContacts();
+    const index = contacts.findIndex((contact) => contact.id === contactId);
+
+    if (index === -1) {
+      return null;
+    }
+    contacts[index] = {contactId, name, email, phone};
+
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+
+    return contacts[index];
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 module.exports = {
   listContacts,
