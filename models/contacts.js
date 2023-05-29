56caa652b2244cabcfe8cae5328b6355f83b82d5
yulyarudenko88 <111283521+yulyarudenko88@users.noreply.github.com>
2023-05-29 23:14:19 +0300

    ADD: functions

--- models/contacts.js
+++ models/contacts.js
@@ -1,12 +1,68 @@
-// const fs = require('fs/promises')
+const fs = require("fs").promises;
+const path = require("path");
+const { nanoid } = require("nanoid");
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname, "db", "contacts.json");
 
-const getContactById = async (contactId) => {}
+async function listContacts() {
+  try {
+    const contacts = await fs.readFile(contactsPath);
 
-const removeContact = async (contactId) => {}
+    return JSON.parse(contacts);
+  } catch (error) {
+    console.log(error);
+  }
+}
 
-const addContact = async (body) => {}
+async function getContactById(contactId) {
+  try {
+    const contacts = await listContacts();
+    const contact = contacts.find((contact) => contact.id === contactId);
+
+    return contact || null;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function removeContact(contactId) {
+  try {
+    const contacts = await listContacts();
+    const index = contacts.findIndex((contact) => contact.id === contactId);
+
+    if (index === -1) {
+      return null;
+    }
+
+    const [deletedContact] = contacts.splice(index, 1);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+
+    return deletedContact;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function addContact(name, email, phone) {
+  try {
+    const contacts = await listContacts();
+
+    const newContact = {
+      id: nanoid(),
+      name,
+      email,
+      phone,
+    };
+
+    contacts.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+
+    return newContact;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 
 const updateContact = async (contactId, body) => {}
 
