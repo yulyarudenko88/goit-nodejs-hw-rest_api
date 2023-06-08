21f083d8862c836ab4805f797e4ef4cf23b52ec6
yulyarudenko88 <111283521+yulyarudenko88@users.noreply.github.com>
2023-06-08 17:48:29 +0300

    UPDATE: add new error message

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -3,12 +3,19 @@ const { HttpError } = require("../helpers");
 const validateBody = (schema) =>
   function validation(req, res, next) {
     const { error } = schema.validate(req.body);
+
     if (error) {
       const missingField = error.details[0].path;
-      const errorMessage =
-        Object.keys(req.body).length === 3
-          ? `missing required ${missingField[0]} field`
-          : "missing fields";
+      let errorMessage = "missing fields";
+
+      if (Object.keys(req.body).length === 3)
+        errorMessage = `missing required ${missingField[0]} field`;
+      else if (
+        missingField[0] === "favorite" &&
+        Object.keys(req.body).length === 0
+      ) {
+        errorMessage = `missing field ${missingField[0]}`;
+      }
 
       next(HttpError(400, errorMessage));
     }
