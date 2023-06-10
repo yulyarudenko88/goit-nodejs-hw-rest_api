872b3ac577593744bfa6ef3334a471320dbff1de
yulyarudenko88 <111283521+yulyarudenko88@users.noreply.github.com>
2023-06-10 10:33:08 +0300

    ADD: dotenv package

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const mongoose = require("mongoose");
 const app = require("./app");
 
-const DB_HOST =
-  "mongodb+srv://Yuliia:UtX1RVWqtg7bcpqy@cluster0.jxkuzgg.mongodb.net/db-contacts?retryWrites=true&w=majority";
+const {DB_HOST} = process.env;
+// console.log(process.env)
+
+// const DB_HOST =
+//   "mongodb+srv://Yuliia:UtX1RVWqtg7bcpqy@cluster0.jxkuzgg.mongodb.net/db-contacts?retryWrites=true&w=majority";
 
 mongoose
   .connect(DB_HOST)
