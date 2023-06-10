0d0d588e9dc9ed105bb2c1be9acc75adbe86c03b
yulyarudenko88 <111283521+yulyarudenko88@users.noreply.github.com>
2023-06-10 14:17:51 +0300

    CREATE: joi schema for users

--- schemas/users/index.js
+++ schemas/users/index.js
@@ -1,3 +1,4 @@
 const usersSchemaMongoose = require("./usersMongoose");
+const usersSchemaJoi = require("./usersMongoose");
 
-module.exports = { usersSchemaMongoose, };
+module.exports = { usersSchemaMongoose, usersSchemaJoi };
