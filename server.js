01bcd9d6c694cba00e71835e65bbb8fe2edca281
yulyarudenko88 <111283521+yulyarudenko88@users.noreply.github.com>
2023-06-10 10:39:12 +0300

    ADD: process.env DB_HOST and PORT

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
 const mongoose = require("mongoose");
 const app = require("./app");
 
-const {DB_HOST} = process.env;
-// console.log(process.env)
-
-// const DB_HOST =
-//   "mongodb+srv://Yuliia:UtX1RVWqtg7bcpqy@cluster0.jxkuzgg.mongodb.net/db-contacts?retryWrites=true&w=majority";
+const { DB_HOST, PORT = 3000 } = process.env;
 
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(3000);
+    app.listen(PORT);
     console.log("Database connection successful");
   })
   .catch((error) => {
