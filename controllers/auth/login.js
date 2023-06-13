a3c7aaa52b8143e7eaf46cce284a841920a59361
yulyarudenko88 <111283521+yulyarudenko88@users.noreply.github.com>
2023-06-13 20:21:16 +0300

    ADD: jsonwebtoken package

--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -1,11 +1,25 @@
-// const bcrypt = require("bcryptjs")
+const bcrypt = require("bcryptjs");
 
-// const { User } = require("../../models");
+const { User } = require("../../models");
 
-// const { ctrlWrapper, HttpError } = require("../../helpers");
+const { ctrlWrapper, HttpError } = require("../../helpers");
 
 const login = async (req, res) => {
-  
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) throw HttpError(401, "Email or password is wrong");
+
+  const passwordCompare = await bcrypt.compare(password, user.password);
+  if (!passwordCompare) throw HttpError(401, "Email or password is wrong");
+
+  const token = "";
+
+  res
+    .status(201)
+    .json({
+      token,
+      user: { email: user.email, subscription: user.subscription },
+    });
 };
 
 module.exports = {
