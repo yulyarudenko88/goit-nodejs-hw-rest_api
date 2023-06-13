518aeced30840289fb32902596da9d77b95f8cbe
yulyarudenko88 <111283521+yulyarudenko88@users.noreply.github.com>
2023-06-13 18:53:48 +0300

    ADD: message error

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,13 +1,18 @@
-const {User} = require("../../models");
+const { User } = require("../../models");
 
-const { ctrlWrapper } = require("../../helpers");
+const { ctrlWrapper, HttpError } = require("../../helpers");
 
 const register = async (req, res) => {
+  const { email } = req.body;
+  const user = await User.findOne({ email });
+  if (user) throw HttpError(409, "Email in use");
   // console.log(req.body);
   // console.log(User)
   const newUser = await User.create(req.body);
   // console.log(newUser);
-  res.status(201).json({ email: newUser.email, subscription: newUser.subscription, });
+  res
+    .status(201)
+    .json({ email: newUser.email, subscription: newUser.subscription });
 };
 
 module.exports = {
