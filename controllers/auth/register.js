e2671bb49769f94862a47785fff1d2c1cc252904
yulyarudenko88 <111283521+yulyarudenko88@users.noreply.github.com>
2023-06-13 19:15:40 +0300

    ADD: password hashing

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,15 +1,18 @@
+const bcrypt = require("bcryptjs")
+
 const { User } = require("../../models");
 
 const { ctrlWrapper, HttpError } = require("../../helpers");
 
 const register = async (req, res) => {
-  const { email } = req.body;
+  const { email, password } = req.body;
   const user = await User.findOne({ email });
   if (user) throw HttpError(409, "Email in use");
-  // console.log(req.body);
-  // console.log(User)
-  const newUser = await User.create(req.body);
-  // console.log(newUser);
+  
+  const hashPassword = await bcrypt.hash(password, 10);
+
+  const newUser = await User.create({...req.body, password: hashPassword});
+  
   res
     .status(201)
     .json({ email: newUser.email, subscription: newUser.subscription });
