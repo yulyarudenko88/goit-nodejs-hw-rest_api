70a3e8a95c9ff82ed5784381dc72c8018b531004
yulyarudenko88 <111283521+yulyarudenko88@users.noreply.github.com>
2023-06-14 22:42:45 +0300

    ADD: logout controller

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -1,5 +1,6 @@
 const { register } = require("./register");
 const { login } = require("./login");
 const { getCurrentUser } = require("./getCurrentUser");
+const { logout } = require("./logout");
 
-module.exports = { register, login, getCurrentUser };
+module.exports = { register, login, getCurrentUser, logout };
---
+++ controllers/auth/logout.js
@@ -0,0 +1,14 @@
+const { User } = require("../../models");
+
+const { ctrlWrapper } = require("../../helpers");
+
+const logout = async (req, res) => {
+  const { _id: id } = req.user;
+  await User.findByIdAndUpdate(id, { token });
+
+  res.status(204).json({ message: "No Content" });
+};
+
+module.exports = {
+  logout: ctrlWrapper(logout),
+};
