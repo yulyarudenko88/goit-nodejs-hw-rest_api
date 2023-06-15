6bcad698bd262f05f7f5b1f74b8ff579765b19b9
yulyarudenko88 <111283521+yulyarudenko88@users.noreply.github.com>
2023-06-15 14:13:31 +0300

    ADD: filtration

--- controllers/contacts/getListContacts.js
+++ controllers/contacts/getListContacts.js
@@ -4,15 +4,15 @@ const { ctrlWrapper } = require("../../helpers");
 
 const getListContacts = async (req, res) => {
   const { _id: owner } = req.user;
-  // console.log(req.query);
-  const { page = 1, limit = 20 } = req.query;
+  
+  const { page = 1, limit = 20, ...query } = req.query;
   const skip = (page - 1) * limit;
 
-  const result = await Contact.find({ owner }, "-createdAt -updatedAt", {
+  const result = await Contact.find({ owner, ...query }, "-createdAt -updatedAt", {
     skip,
     limit,
   }).populate("owner", "email subscription");
-  
+
   res.json(result);
 };
 
