8c30f16d185ce545541231876961943296cd14a0
yulyarudenko88 <111283521+yulyarudenko88@users.noreply.github.com>
2023-06-15 13:39:01 +0300

    DELETE: commits

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -7,17 +7,14 @@ const { HttpError } = require("../helpers");
 const { SECRET_KEY } = process.env;
 
 const authenticate = async (req, res, next) => {
-  const { authorization = "" } = req.headers;
-  // console.log(authorization);
+  const { authorization = "" } = req.headers;  
   const [bearer, token] = authorization.split(" ");
 
   if (bearer !== "Bearer") next(HttpError(401, "Not authorized"));
 
   try {
     const { id } = jwt.verify(token, SECRET_KEY);
-    // console.log(id);
     const user = await User.findById(id);
-    // console.log(user);
     if (!user || !user.token) next(HttpError(401, "Not authorized"));
 
     req.user = user;
