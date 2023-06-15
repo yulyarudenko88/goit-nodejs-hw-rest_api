186258349cd92fdf8eeffcf77f15dc1702bfbee5
yulyarudenko88 <111283521+yulyarudenko88@users.noreply.github.com>
2023-06-15 14:03:07 +0300

    ADD: pagination

--- controllers/contacts/getListContacts.js
+++ controllers/contacts/getListContacts.js
@@ -4,10 +4,15 @@ const { ctrlWrapper } = require("../../helpers");
 
 const getListContacts = async (req, res) => {
   const { _id: owner } = req.user;
-  const result = await Contact.find({ owner }).populate(
-    "owner",
-    "email subscription"
-  );
+  // console.log(req.query);
+  const { page = 1, limit = 20 } = req.query;
+  const skip = (page - 1) * limit;
+
+  const result = await Contact.find({ owner }, "-createdAt -updatedAt", {
+    skip,
+    limit,
+  }).populate("owner", "email subscription");
+  
   res.json(result);
 };
 
