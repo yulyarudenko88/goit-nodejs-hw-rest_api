6edc09bdfaf1ffe405de3f6ec94f365be3315e45
yulyarudenko88 <111283521+yulyarudenko88@users.noreply.github.com>
2023-06-15 12:29:58 +0300

    FIX: grammar mistake

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -8,18 +8,19 @@ const { SECRET_KEY } = process.env;
 
 const authenticate = async (req, res, next) => {
   const { authorization = "" } = req.headers;
-  console.log(authorization);
+  // console.log(authorization);
   const [bearer, token] = authorization.split(" ");
 
   if (bearer !== "Bearer") next(HttpError(401, "Not authorized"));
 
   try {
     const { id } = jwt.verify(token, SECRET_KEY);
-    const user = await User.findBuId(id);
+    // console.log(id);
+    const user = await User.findById(id);
+    // console.log(user);
     if (!user || !user.token) next(HttpError(401, "Not authorized"));
 
     req.user = user;
-    console.log(req.user);
   } catch {
     next(HttpError(401, "Not authorized"));
   }
