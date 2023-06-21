b4be7dba65b019fd0babc0ba571e9593335133bd
yulyarudenko88 <111283521+yulyarudenko88@users.noreply.github.com>
2023-06-21 19:24:33 +0300

    UPDATE: generatinga link for avatar

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,35 +1,28 @@
 const bcrypt = require("bcryptjs");
-const fs = require("fs").promises;
-const path = require("path");
+const gravatar = require("gravatar");
 
 const { User } = require("../../models");
 
 const { ctrlWrapper, HttpError } = require("../../helpers");
 
-const avatarsDir = path.resolve("public", "avatars");
-
 const register = async (req, res) => {
-  // console.log(req.body);
-  // console.log(req.file);
-
   const { email, password } = req.body;
-  const { path: oldPath, filename } = req.file;
-
-  const newPath = path.join(avatarsDir, filename);
-  await fs.rename(oldPath,newPath);
 
   const user = await User.findOne({ email });
   if (user) throw HttpError(409, "Email in use");
 
   const hashPassword = await bcrypt.hash(password, 10);
+  const avatarURL = await gravatar.url(email, { protocol: 'http', s: "250" });
 
-  const newUser = await User.create({ ...req.body, password: hashPassword });
+  const newUser = await User.create({
+    ...req.body,
+    password: hashPassword,
+    avatarURL,
+  });
 
-  res
-    .status(201)
-    .json({
-      user: { email: newUser.email, subscription: newUser.subscription },
-    });
+  res.status(201).json({
+    user: { email: newUser.email, subscription: newUser.subscription },
+  });
 };
 
 module.exports = {
