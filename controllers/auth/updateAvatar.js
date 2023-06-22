cb149ce01959d8961b6d9fb67a3f9a0b02de4527
yulyarudenko88 <111283521+yulyarudenko88@users.noreply.github.com>
2023-06-22 15:03:51 +0300

    ADD: jimp package

--- controllers/auth/updateAvatar.js
+++ controllers/auth/updateAvatar.js
@@ -1,9 +1,24 @@
+const fs = require('fs').promises
+const path = require('path')
+
 const { User } = require("../../models");
 
 const { HttpError, ctrlWrapper } = require("../../helpers");
 
+// const avatarsDir = path.resolve("public", "movies");
+
 const updateAvatar = async (req, res, next) => {
   console.log(req.file)
+
+  // const { _id: owner } = req.user;
+  //   const { path: oldPath, filename } = req.file;
+  //   const newPath = path.join(moviesDir, filename);
+  //   await fs.rename(oldPath, newPath);
+  //   const poster = path.join("movies", filename);
+  //   const result = await Movie.create({...req.body, poster, owner});
+  //   res.status(201).json(result);
+
+
 //   const { subscription: newSubscription } = req.body;
 //   const { _id: id, subscription, email } = req.user;
 
