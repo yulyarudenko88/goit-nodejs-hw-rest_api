fca1c5a855e49798fb8efbf7638972d09e76b00d
yulyarudenko88 <111283521+yulyarudenko88@users.noreply.github.com>
2023-06-23 13:26:49 +0300

    ADD: controller for updating avatar

--- controllers/auth/updateAvatar.js
+++ controllers/auth/updateAvatar.js
@@ -1,38 +1,32 @@
-const fs = require('fs').promises
-const path = require('path')
+const fs = require("fs").promises;
+const path = require("path");
 
 const { User } = require("../../models");
 
 const { HttpError, ctrlWrapper } = require("../../helpers");
 
-// const avatarsDir = path.resolve("public", "movies");
+const avatarsDir = path.resolve("public", "avatars");
 
 const updateAvatar = async (req, res, next) => {
-  console.log(req.file)
+  const { _id: id } = req.user;
 
-  // const { _id: owner } = req.user;
-  //   const { path: oldPath, filename } = req.file;
-  //   const newPath = path.join(moviesDir, filename);
-  //   await fs.rename(oldPath, newPath);
-  //   const poster = path.join("movies", filename);
-  //   const result = await Movie.create({...req.body, poster, owner});
-  //   res.status(201).json(result);
+  if (!req.file) throw HttpError(401, "Not authorized");
+  const { path: oldPath, filename } = req.file;
 
+  const newPath = path.join(avatarsDir, filename);
+  await fs.rename(oldPath, newPath);
 
-//   const { subscription: newSubscription } = req.body;
-//   const { _id: id, subscription, email } = req.user;
+  const avatar = path.join("avatars", filename);
 
-//   if (newSubscription === subscription)
-//     throw HttpError(
-//       400,
-//       `User ${email} already has ${subscription} subscription`
-//     );
+  const { avatarURL } = await User.findByIdAndUpdate(
+    id,
+    { avatarURL: avatar },
+    {
+      new: true,
+    }
+  );
 
-//   const result = await User.findByIdAndUpdate(id, req.body, {
-//     new: true,
-//   });
-
-//   res.status(200).json(result);
+  res.status(200).json({ avatarURL });
 };
 
 module.exports = {
