d4643f4cc619f9dfdb4223f8f4ebd919eefccbfa
yulyarudenko88 <111283521+yulyarudenko88@users.noreply.github.com>
2023-06-26 15:23:03 +0300

    UPDATE: sending verification email

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,9 +1,13 @@
+require("dotenv").config();
 const bcrypt = require("bcryptjs");
 const gravatar = require("gravatar");
+const {nanoid} = require("nanoid");
+
+const { BASE_URL } = process.env;
 
 const { User } = require("../../models");
 
-const { ctrlWrapper, HttpError } = require("../../helpers");
+const { ctrlWrapper, HttpError, sendEmail } = require("../../helpers");
 
 const register = async (req, res) => {
   const { email, password } = req.body;
@@ -12,13 +16,23 @@ const register = async (req, res) => {
   if (user) throw HttpError(409, "Email in use");
 
   const hashPassword = await bcrypt.hash(password, 10);
-  const avatarURL = await gravatar.url(email, { protocol: 'http', s: "250" });
-
+  const avatarURL = await gravatar.url(email, { protocol: "http", s: "250" }); 
+  const verificationToken = nanoid();
+ 
   const newUser = await User.create({
     ...req.body,
     password: hashPassword,
     avatarURL,
+    verificationToken,
   });
+
+  const verifyEmail = {
+    to: email,
+    subject: "Verify your email",
+    html: `<a href="${BASE_URL}/users/verify/${verificationToken}" target="_blank" rel="noreferrer noopener">Click to verify email</a>`,
+  };
+
+  await sendEmail(verifyEmail);
 
   res.status(201).json({
     user: { email: newUser.email, subscription: newUser.subscription },
